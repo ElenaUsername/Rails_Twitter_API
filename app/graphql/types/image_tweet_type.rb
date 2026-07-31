f7a5e6602b0f4268c14a5e35effca1543929a27f
Bzovii Elena <bzovii.elena2024@gmail.com>
2026-07-31 22:27:54 +0300

    Completed with test for types and renamed tweet_image_type

--- app/graphql/types/image_tweet_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class ImageTweetType < Types::BaseObject
-    graphql_name "Image"
-
-    field :url, String, null: false
-    field :byte_size, Integer, null: false
-  end
-end
--- spec/graphql/types/tweet_type_spec.rb
+++ spec/graphql/types/tweet_type_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.describe Types::TweetType do
   subject(:tweet_type) { described_class }
 
-  it "has the correct graphql name" do
+  it "has the correct graphql name Tweet" do
     expect(tweet_type.graphql_name).to eq("Tweet")
   end
 
-  it "verify the expected fields existance" do
+  it "verify the expected fields existance uuid" do
     expect(tweet_type.fields.keys).to contain_exactly(
       "uuid",
     )
